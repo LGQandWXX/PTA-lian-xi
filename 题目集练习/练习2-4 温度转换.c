6e2fcfb8c1fc13044ff72c619e146fa6c3fbad19
XwX阿牛 <124447930+LGQandWXX@users.noreply.github.com>
2023-02-24 15:59:33 +0800

    Delete 题目集练习 directory

--- 题目集练习/练习2-4 温度转换.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	int fahr = 150;
-	int c;
-	c = 5*(fahr-32)/9;
-	printf("fahr = 150, celsius = %d",c);
-	return 0;
-}
--- 题目集练习/练习2-6 计算物体自由下落的距离.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	double h;
-	h = 0.5*10*3*3;; 
-	printf("height = %.2f",h);
-	return 0;
-}
